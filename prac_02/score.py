a4fbcf922589b836cb3541691e3303168a6d1e03
tuandao <ngoctuan.dao@my.jcu.edu.au>
2023-07-22 10:10:42 +0800

    Refactor score program to use functions, add random score.

--- prac_02/score.py
+++ prac_02/score.py
@@ -5,12 +5,28 @@ Broken program to determine score status
 MAX_SCORE = 100
 MIN_SCORE = 0
 
-score = float(input("Enter score: "))
-if score < MIN_SCORE or score > MAX_SCORE:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+import random
+
+
+def main():
+    score = float(input("Enter score: "))
+    message = determine_score(score)
+    print(message)
+    random_score = random.randint(1, 100)
+    message2 = determine_score(random_score)
+    print(f"The random score is {random_score} which is {message2}")
+
+
+def determine_score(score):
+    if score < MIN_SCORE or score > MAX_SCORE:
+        message = "Invalid score"
+    elif score >= 90:
+        message = "Excellent"
+    elif score >= 50:
+        message = "Passable"
+    else:
+        message = "Bad"
+    return message
+
+
+main()
