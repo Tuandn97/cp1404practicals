c386796ca0d6c95fe989e5b82f7bb63242284835
tuandao <ngoctuan.dao@my.jcu.edu.au>
2023-07-29 11:47:17 +0800

    Question 2: read the  name from  file name.txt and print

--- prac_03/files.py
+++ prac_03/files.py
@@ -5,3 +5,9 @@ user_name = input("Please enter your name: ")
 with open(FILENAME, 'w') as out_file:
     print(user_name, file=out_file)
 
+"""2. Write code that opens "name.txt" and reads the name (as above) then prints,
+Your name is Bob"""
+
+with open(FILENAME) as in_file:
+    name = in_file.read().strip()
+    print(f"Your name is {name}")
