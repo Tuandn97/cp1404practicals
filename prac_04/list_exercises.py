c97fe0a1a7a48a084c4309096dd9ad1b03070d08
tuandao <ngoctuan.dao@my.jcu.edu.au>
2023-08-01 13:10:25 +0800

    Upload the answer for the Woefully inadequate security checker

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,6 +1,7 @@
 """Basic list operations"""
 
 numbers = []
+
 for number in range(5):
     number = int(input("Number: "))
     numbers.append(number)
@@ -9,3 +10,14 @@ print(f"The last number is: {numbers[-1]}")
 print(f"The smallest number is: {min(numbers)}")
 print(f"The largest number is: {max(numbers)}")
 print(f"The average of the number is: {sum(numbers)/len(numbers)}")
+
+""" Woefully inadequate security checker """
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+username = input("Enter your username: ")
+if username in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
