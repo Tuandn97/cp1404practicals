b921077fab024c24a5ad7aa155c9d71ba644b0d0
tuandao <ngoctuan.dao@my.jcu.edu.au>
2023-08-26 16:47:07 +0800

    Edit the format

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -2,6 +2,8 @@
 CP1404/CP5632 Practical - Client code to use the Car class.
 Note that the import has a folder (module) in it.
 This is why we name our folders with no spaces or capitals, as valid module names.
+Estimate: 10 minutes
+Actual:   10 minutes
 """
 
 from prac_06.car import Car
@@ -9,9 +11,15 @@ from prac_06.car import Car
 
 def main():
     """Demo test code to show how to use car class."""
-    my_car = Car(180)
+    limo = Car("Limo", 100)
+    limo.add_fuel(20)
+    print(limo.fuel)
+    limo.drive(115)
+    print(limo)
+
+    my_car = Car("My car", 180)
     my_car.drive(30)
-    print(f"Car has fuel: {my_car.fuel}")
+    print(f"fuel: {my_car.fuel}")
     print(my_car)
 
 
