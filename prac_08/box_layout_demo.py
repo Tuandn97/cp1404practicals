e6f4bf9f250a244dbedef2aff520de77841109f2
tuandao <ngoctuan.dao@my.jcu.edu.au>
2023-09-04 14:46:16 +0800

    update the clear text requirement

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -10,8 +10,11 @@ class BoxLayoutDemo(App):
 
     def handle_greet(self):
         print('greet')
-        self.root.ids.output_label.text = "Hello "
         self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
+
+    def handle_clear(self):
+        self.root.ids.output_label.text = ""
+        self.root.ids.input_name.text = ""
 
 
 BoxLayoutDemo().run()
