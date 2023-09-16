5aae8a6609cb7f2baa407a8e15bee95ca25f2411
tuandao <ngoctuan.dao@my.jcu.edu.au>
2023-09-16 10:47:50 +0800

    Create 2 files of the silver_service_taxi

---
+++ prac_09/silver_service_taxi.py
@@ -0,0 +1,6 @@
+"""
+CP1404 - Programming 2
+SilverServiceTaxi Class
+Name: Doa Ngoc Tuan
+Start from: 10:46 am  - Sat 16 Sep 2023
+"""
---
+++ prac_09/silver_service_taxi_test.py
@@ -0,0 +1,6 @@
+"""
+CP1404 - Programming 2
+SilverServiceTaxi Client
+Name: Doa Ngoc Tuan
+Start from:  am  - Sat 16 Sep 2023
+"""
