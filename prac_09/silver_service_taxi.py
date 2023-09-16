af17af4487a9fc90d4a65ed66dc68ba42a01ba0b
tuandao <ngoctuan.dao@my.jcu.edu.au>
2023-09-16 11:37:48 +0800

    Fixed typo error of the SilverServiceTaxi Class, and reformat the file

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -8,19 +8,20 @@ from prac_09.taxi import Taxi
 
 
 class SilverServiceTaxi(Taxi):
-    """ Main of SilverServiceTaxi Class"""
-    flag_fall = 4.50
+    """Main SilverServiceTaxi Class"""
 
-    def __init(self, name, fuel, fanciness: float):
-        """Initialise a SilverServiceTaxi instance, based on parent class Car"""
+    flagfall = 4.50
+
+    def __init__(self, name, fuel, fanciness: float):
+        """Initialize a SilverServiceTaxi instance"""
         super().__init__(name, fuel)
         self.fanciness = fanciness
         self.price_per_km *= fanciness
 
     def __str__(self):
-        """Return a string for SilverServiceTaxi class   """
-        return f"{super().__init__} ${self.fanciness}/km plus flag-fall of {self.flag_fall}"
+        """Return a string representation of SilverServiceTaxi"""
+        return f"{super().__str__()} ${self.fanciness:.2f}/km plus flag-fall of ${self.flagfall:.2f}"
 
-    def __get_fare__(self):
-        """Add flag_all to the fare"""
-        return super().get_fare() + self.flag_fall
+    def get_fare(self):
+        """Calculate the fare for the taxi ride, including flagfall"""
+        return super().get_fare() + self.flagfall
